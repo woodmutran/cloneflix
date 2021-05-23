4a2bb9505f91852babf181352cca0df8346a37ba
Wood Mutran <woodmutran@gmail.com>
2021-05-23 19:02:19 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import './App.css'
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow/';
+import FeaturedMovie from './components/FeaturedMovie/';
+import Header from './components/Header/'
+
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setfeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegando o Feature
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setfeaturedData(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
+
+  return (
+    <div className="page">
+
+      <Header black={blackHeader} />
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+
+      <footer>
+        {/* Feito com <span role="img" aria-label="coracao">♥ por Wood Mutran</span> */}
+        Feito para estudo de React. Direitos de imagem para Netflix.
+        Dados extraidos de <a href="https://www.themoviedb.org/" target="_blank">TMDB</a>
+      </footer>
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+        </div>
+      }
+    </div>
+  );
+}
+
+// export default useEffect();
